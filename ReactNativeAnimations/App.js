5e95a1ddd27d62c37d0afe803d99c986f6d6ae84
Yash Rajgor <yash592@gmail.com>
2019-06-27 16:29:43 -0700

    Animated ball

--- ReactNativeAnimations/App.js
+++ ReactNativeAnimations/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  PanResponder,
+  Animated,
+  Image
+} from "react-native";
 import Ball from "./src/components/Ball";
 
 class App extends Component {
+  position = new Animated.ValueXY();
+
+  panResponder = new PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (e, gesture) => {
+      this.position.setValue({ x: gesture.dx, y: gesture.dy });
+    },
+    onPanResponderEnd: (e, gesture) => {
+      Animated.spring(this.position, { toValue: 0 }).start();
+    }
+  });
+
+  state = {
+    panResponder: this.panResponder,
+    position: this.position
+  };
+
   render() {
+    console.log(this.panResponder.panHandlers);
+    console.log("position", this.position);
     return (
       <View style={styles.container}>
-        <Ball />
+        <Animated.View
+          style={this.state.position.getLayout()}
+          {...this.state.panResponder.panHandlers}
+        >
+          <Image
+            source={{ uri: "https://i.imgur.com/67jPyVI.png" }}
+            style={{ width: 400, height: 400 }}
+            resizeMode={"contain"}
+          />
+        </Animated.View>
       </View>
     );
   }
@@ -15,7 +50,9 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
